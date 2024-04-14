584d435ae249fc40532c735a35fe4a38dec4d2f1
uncle-gua <scnb2681@163.com>
2024-04-14 23:06:45 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module watcher
+
+go 1.22.1
+
+require (
+	github.com/robfig/cron/v3 v3.0.1
+	github.com/uncle-gua/gobinance v0.0.0-20240403123736-f357eb8703ff
+	github.com/uncle-gua/log v0.0.0-20221204022824-ca8d28e70377
+)
+
+require (
+	github.com/bitly/go-simplejson v0.5.1 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/jpillora/backoff v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/uncle-gua/wsc v0.0.0-20231130105340-37c38a631342 // indirect
+	golang.org/x/net v0.19.0 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/robfig/cron/v3"
+	"github.com/uncle-gua/gobinance/futures"
+	"github.com/uncle-gua/log"
+)
+
+type Account struct {
+	Name      string  `json:"name"`
+	ApiKey    string  `json:"apiKey"`
+	ApiSecret string  `json:"apiSecret"`
+	Profit    float64 `json:"profit"`
+	Leverage  float64 `json:"leverage"`
+	Spec      string  `json:"spec"`
+}
+
+func main() {
+	body, err := os.ReadFile("./config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var accounts []Account
+	if err := json.Unmarshal(body, &accounts); err != nil {
+		log.Fatal(err)
+	}
+
+	for _, account := range accounts {
+		c := cron.New(cron.WithSeconds())
+		c.AddFunc(account.Spec, func() {
+			if leverage, err := run(account); err != nil {
+				log.Error(err)
+			} else {
+				if leverage < account.Leverage {
+					c.Stop()
+				}
+			}
+		})
+		c.Start()
+	}
+
+	chExit := make(chan struct{})
+	go func(ch chan struct{}) {
+		chSignal := make(chan os.Signal, 1)
+		signal.Notify(chSignal, syscall.SIGINT, syscall.SIGTERM)
+
+		<-chSignal
+		close(ch)
+	}(chExit)
+
+	<-chExit
+}
+
+func run(a Account) (float64, error) {
+	leverage := 0.0
+	client := futures.NewClient(a.ApiKey, a.ApiSecret)
+	account, err := client.NewGetAccountService().Do(context.Background())
+	if err != nil {
+		return leverage, err
+	}
+
+	total := 0.0
+	for _, position := range account.Positions {
+		if position.PositionAmt > 0 {
+			value := position.PositionAmt * position.EntryPrice
+			roe := position.UnrealizedProfit / value * 100
+			if roe > a.Profit {
+				// if _, err := client.NewCreateOrderService().
+				// 	Symbol(position.Symbol).
+				// 	Type(futures.OrderTypeMarket).
+				// 	Side(futures.SideTypeSell).
+				// 	PositionSide(futures.PositionSideTypeLong).
+				// 	Quantity(fmt.Sprintf("%f", position.PositionAmt)).
+				// 	Do(context.Background()); err != nil {
+				// 	return leverage, err
+				// }
+				log.Infof("account: %s, symbol: %s, pnl: %.2f, roe: %.2f", a.Name, position.Symbol, position.UnrealizedProfit, roe)
+			} else {
+				total += value
+			}
+		}
+	}
+
+	leverage = total / account.TotalWalletBalance
+	log.Infof("account: %s, leverage: %.2f", a.Name, leverage)
+	return leverage, err
+}
